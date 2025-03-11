329e05f85aaffd639876dc7a9139460c5e16cd34
Kerry Back <kerryback@gmail.com>
2025-03-11 08:55:37 -0500

    day 1 and schedule

---
+++ clean.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+latex_dir = 'latex'
+docs_dir = 'docs'
+
+# Get list of files in latex directory
+files = os.listdir(latex_dir)
+
+# Delete files that aren't .tex or .pdf
+for file in files:
+    if not file.endswith(('.tex', '.pdf')):
+        os.remove(os.path.join(latex_dir, file))
+
+# Copy PDF files to docs directory
+for file in files:
+    if file.endswith('.pdf'):
+        shutil.copy2(os.path.join(latex_dir, file), os.path.join(docs_dir, file))
