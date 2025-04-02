23f7e2887256ffdd55df0584526f501b40d1bfe2
Kerry Back <kerryback@gmail.com>
2025-04-02 11:59:09 -0500

    python installation

---
+++ docs/assets/cost_of_equity_app/test.py
@@ -0,0 +1,12 @@
+import pandas
+import numpy
+import matplotlib
+import yfinance
+import streamlit
+import seaborn
+import scipy
+import statsmodels
+import pptx
+import lxml
+
+print("Ready to Go!")
